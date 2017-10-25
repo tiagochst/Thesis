0ded9b120ce25a9910e4e479f3d722480b1de899
Tiago Chedraoui Silva <tiagochst@gmail.com>
2017-10-25 16:26:26 -0200

    data structures

--- codes/plotpair.R
+++ codes/plotpair.R
@@ -1,22 +1,21 @@
 # by region and without detail about DNA methylation
-  schematic.plot(data = mae, 
-                 pair = Hypo.pair, 
-                 byCoordinate = list(chr="chr1", start = 40000429, end = 42070429))
-               
-# by probe and with detail about DNA methylation              
-schematic.plot(data = mae, 
-               group.col = "definition", 
+schematic.plot(data = mae,
+               pair = Hypo.pair,
+               byCoordinate = list(chr="chr1", start = 40000429, end = 42070429))
+# by probe and with detail about DNA methylation
+schematic.plot(data = mae,
+               group.col = "definition",
                group1 = "Primary solid Tumor",
                group2 = "Solid Tissue Normal",
-               pair = Hypo.pair, 
+               pair = Hypo.pair,
                statehub.tracks = "hg38/ENCODE/mcf-7.16mark.segmentation.bed",
                byProbe = "cg04723436")
-               
-# by gene ID and adding annotation tracks from StateHub               
-schematic.plot(data = mae, 
-               group.col = "definition", 
+
+# by gene ID and adding annotation tracks from StateHub
+schematic.plot(data = mae,
+               group.col = "definition",
                group1 = "Primary solid Tumor",
                group2 = "Solid Tissue Normal",
-               pair = Hypo.pair, 
+               pair = Hypo.pair,
                byGeneID = "ENSG00000107485", # GATA3
                statehub.tracks = "hg38/ENCODE/mcf-7.16mark.segmentation.bed")
